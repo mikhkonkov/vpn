9aa927616232f2a37b9ad19abc8cb98dffd3639f
Konkov Mikhail <mikhail.konkov@dualbootpartners.com>
2025-01-30 13:28:58 +0400

    small fixes

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,8 +4,8 @@ provider "digitalocean" {
 
 resource "digitalocean_droplet" "vpn_server" {
   name   = "vpn-server"
-  region = "ams3"
-  size   = "s-1vcpu-1gb" # (Shared CPU, $4/month)
+  region = "ams3" # Amsterdam
+  size   = "s-1vcpu-1gb" # (Shared CPU, $6/month)
   image  = "ubuntu-22-04-x64"
 
   ssh_keys = [digitalocean_ssh_key.key.id]
@@ -21,6 +21,6 @@ resource "digitalocean_droplet" "vpn_server" {
 
 resource "digitalocean_ssh_key" "key" {
   name       = "terraform-key"
-  public_key = file("/root/.ssh/id_rsa_do.pub")
+  public_key = file(var.ssh_key_path)
 }
 
